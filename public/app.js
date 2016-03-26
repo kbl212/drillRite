d96b9a953b4ce05db3f66547bb4d5d4a0d94e5d8
kbl212 <kyle.lauritzen@gmail.com>
2016-03-26 03:08:36 -0600

    added images. Routes set up correctly. # taken out of URL

--- public/app.js
+++ public/app.js
@@ -1,1 +1,19 @@
-var app = angular.module('drillRite', []);
+var app = angular.module('drillRite', ['ngRoute']);
+
+app.config(['$routeProvider', '$locationProvider', function ($routeProvider, $locationProvider) {
+
+    $locationProvider.html5Mode(true);
+
+    $routeProvider
+        .when('/home', {
+            templateUrl: 'features/homePage/homeTmpl.html',
+            controller: 'homeCtrl'
+        })
+        .when('/about', {
+            templateUrl: 'features/aboutPage/aboutTmpl.html',
+            controller: 'aboutCtrl'
+        })
+        .otherwise({
+            redirectTo: '/home'
+        })
+}]);
