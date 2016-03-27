c766298916e0e288fd5960bcd0087b2c93fd486a
kbl212 <kyle.lauritzen@gmail.com>
2016-03-26 22:09:02 -0600

    map working correctly. edited styling quite a bit. made footer, working on footer positioning

---
+++ public/features/footer/footerDirective.js
@@ -0,0 +1,9 @@
+angular.module('drillRite')
+.directive('myfooter', function(){
+	return {
+		templateUrl: 'features/footer/footerTmpl.html',
+		link: function(scope, elem, attrs){
+			
+		}
+	}
+})
--- public/features/homePage/homeCtrl.js
+++ public/features/homePage/homeCtrl.js
@@ -3,4 +3,43 @@ var app = angular.module('drillRite');
 
 app.controller('homeCtrl', function($scope) {
     
+            $scope.initMap = function() {
+            var myLatLng = {
+                lat: 40.380370,
+                lng: -111.835317
+            };
+
+            $scope.map = new google.maps.Map(document.getElementById('map'), {
+                zoom: 16,
+                center: myLatLng
+            });
+
+            var marker = new google.maps.Marker({
+                position: myLatLng,
+                map: $scope.map,
+                title: 'DrillRite'
+            });
+        }
+            
+            $scope.initMap();
+    
+    
+    
+    
+                /* var myLatLng = {
+                lat: 40.380370,
+                lng: -111.835317
+            };
+            
+            var map = new google.maps.Map(document.getElementById('map'), {
+                zoom: 15,
+                center: myLatLng
+            });
+
+            var marker = new google.maps.Marker({
+                position: myLatLng,
+                map: map,
+                title: 'Hello World!'
+            });
+        }*/
 });
